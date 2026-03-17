a336fb9dd73ac21a92b65dce14cb27353ccd45b4
MK4MAX44 <wjsehdgur4@gmail.com>
2026-03-17 18:17:34 +0900

    Add files via upload

--- 260310.cpp
+++ 260310.cpp
@@ -6,13 +6,13 @@
 //	return 0;
 //}
 
-#include <stdio.h>
-
-int main()
-{
-	printf("안녕하세요");
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요");
+//	return 0;
+//}
 
 //#include <stdio.h>
 //int main() {
@@ -29,4 +29,119 @@ int main()
 //	printf("안녕하세요");
 //	printf("안녕하세요\n")
 //		return 0;
-//}                      
+//} 
+
+//#include <stdio.h>
+//int main()
+//{
+//	int k;
+//	printf("원하는 숫자를 입력 : ");
+//	scanf("%d", &k); //vs :scanf_s
+//	printf("입력한 숫자는 : %d\n", k );
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("사과 1개\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int apple = 1;
+//	printf("사과 %d개\n", apple);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("키보드 3개\n");
+//	printf("마우스 2개\n");
+//	return 0;
+//
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int keyboard = 3;
+//	int mouse = 2;
+//	printf("키보드: %d개\n, keyboard");
+//	printf{"마우스: %d개\n, mouse"}
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//
+//int main()
+//{
+//	int coin1 = 500;
+//	int coin2 = 1000;
+//	printf("%d + %d = %d\n",
+//		coin1, coin2, coin1 + coin2);
+//	return 0;
+//}
+
+
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	char A;
+//	A = 'A';
+//	printf("A  = %c, A = %d\n", A, A);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	double pi;
+//	pi = 3.14;
+//	float e = 2.718;
+//	printf("pi = %.2lf, e = %.3f.. \n", pi, e);
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+
+	double pi;
+	pi = 3.14;
+	int radius = 5;
+
+	//pi*반지름*반지름
+
+	float extent = pi * radius * radius;
+	printf("원의 넓이: %f ", extent);
+
+	//
+	//int circumference = 25;
+
+
+
+
+	//printf("원의 둘레 = %25pi... , 원의 넓이 = %5pi..\n")
+
+
+
+
+}
+
+
+
+
+
+
