ed2773f6809d24ade6fa115d86ef7b4e01dfc13b
MK4MAX44 <wjsehdgur4@gmail.com>
2026-03-17 21:53:23 +0900

    Add files via upload

---
+++ 20260317.cpp
@@ -0,0 +1,115 @@
+//#include <stdio.h>
+//int main()
+//{
+//
+//	double pi;
+//	pi = 3.14;
+//	int radius = 5;
+//
+//	//pi*반지름*반지름
+//
+//
+//	float extent = pi * radius * radius;
+//
+//	printf("원의 넓이: %f ", extent);
+//
+//	//circumference= 2 * pi * int redius(반지름);
+//
+//	float circumference = 2 * pi * radius;
+//
+//	printf("원의 둘레 : %f", circumference);
+//	
+//	return 0;
+//
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	double area_of_a_circle;
+//	
+//    area_of_a_circle: 
+//
+//	int upper_change = 5;
+//
+//	int height = 7;
+//
+//	int base = 3;
+//	//밑변
+//
+//
+//	upper_change = 5;
+//	//윗변
+//
+//
+//	height = 7;
+//	//높이
+//
+//
+//	area_of_a_circle = (base + upper_change) * height / 2;
+//	//넓이
+//
+//	printf("사다리꼴의 넓이: %f", area_of_a_circle);
+//
+//	return 0;
+//
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int k;
+//	printf("원하는 숫자를 입력 : ");
+//	scanf("%d" , &k);  //vs:scanf_s
+//	printf("입력한 숫자는 : %d\n" , k);
+//		return 0;
+//
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int y, x;
+//	printf("더하고 샆은 값 2개 입력 : ");
+//    scanf("%d %d", &y, &x); //scanf_s
+//	printf("두 값의 합은 %d입니다.\n", y + x);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	char a, b, c;
+//	printf("문자를 두 개 입력: ");
+//	scanf("%c%c%c", &a, &b, &c);
+//	printf("문자출력: %c%c%c\n",a,b,c);
+//	return 0;
+//
+//}
+
+#include <stdio.h>
+int main()
+{
+	int radius;//반지름=radiu
+	int r; //3.14*r제곱
+	printf("원의 넓이:");
+	scanf("%d", &radius);
+	printf("원의 길이:");//원의 길이 2r
+	
+
+
+	
+
+
+
+
+
+
+
+
+}
+
