010c0efe70ea297e2747145232ff914ebe205b1e
MK4MAX44 <wjsehdgur4@gmail.com>
2026-04-28 19:51:44 +0900

    Add files via upload

--- 20260317.cpp
+++ 20260317.cpp
@@ -502,10 +502,76 @@ int main()
 //}
 
 
-#include <Stdio.h>
-#include <Stdlib.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//
+//	int randint = rand() % 10;
+//	int nextInt = rand() % 10;
+//
+//	printf("첫숫자: %d\n", randint);
+//	printf("다음숫자: %d", nextInt);
+//
+//	return 0;
+//
+//}
 
-int main()
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+//int main()
+//{
+//	srand((unsigned int)time(NULL));
+//	//unsigned int 0~42억 
+//	// 시간은 항상 양수여서 NULL
+//
+//	int randInt = rand() % 10;
+//	int nextInt = rand() % 10;
+//	int afterInt = (rand() % 10) + 50;
+//	printf("첫숫자: %d\n", randInt);// 0~9
+//	printf("다음숫자: %d\n", nextInt);// 0~9
+//	printf("그 다음숫자 : %d", afterInt);// 50~59
+//
+//	return 0;
+//
+//
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+//int main()
+//{
+//	srand((unsigned int)time(NULL));
+//
+//	printf("내 주머니에 있는 돈은");
+//
+//	int myMoney = (rand() % 10 + 1) * 1000; // rand() % 10= 0~9 거기서 +1했으니깐 1~10 그리고 곱하면 1000~10000이다 
+//
+//	printf("%d원이다.\n", myMoney);
+//
+//	int coin500 = rand() % 4 * 500; //0~3 , 500 곱하면 int coin500= 최소0 ~최대1500원
+//	int coin100 = rand() % 5000; //  int coin100 = 0~4999
+//
+//	coin100 = (coin100 / 100) * 100; // )0~49 이고 거기에 100을 곱하면 4900원이다
+//
+//	
+//	printf("그리고 과자의 가격은");
+//	printf("%d원이다.", coin500 + coin100); 
+//	printf("\n나는 과자를 살 수 있을까?");
+//	
+//	return 0;
+//
+//
+//}
+
+
 
 
 
